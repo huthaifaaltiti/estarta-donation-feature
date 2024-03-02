961468074aa50a38e75cffea3bb6dd5a97fba8fc
Huthaifa AlTiti <haltiti@estartasolutions.com>
2024-03-02 15:59:50 +0300

    finishing modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,148 @@
+import React from "react";
+
+const SubmissionModal = ({
+  isOpen,
+  closeModal,
+  selectedOrganization,
+  donationAmount,
+  startDate,
+  endDate,
+  isRecurring,
+}) => {
+  const handleConfirm = async () => {
+    const formData = {
+      ETId: "",
+      EmployeeProfileID: "1255",
+      TransactionID: "",
+      TransactionType: "",
+      Amount: parseFloat(donationAmount),
+      StartDate: startDate,
+      EndDate: endDate,
+      TransactionNote: "",
+      IsMontheyRecurrence: isRecurring,
+    };
+
+    try {
+      const response = await fetch("https://10.2.2.3/Training/Submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        console.log("Donation submitted successfully!");
+      } else {
+        console.error("Failed to submit donation:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+
+    closeModal();
+  };
+
+  return (
+    <div
+      className={`fixed inset-0 z-50 flex items-center justify-center ${
+        isOpen ? "" : "hidden"
+      }`}
+    >
+      <div className="fixed inset-0 bg-black opacity-50"></div>
+      <div className="z-50 bg-[#F9FAFB] w-[930px] h-601 px-[62px] py-[68px] rounded-lg shadow-xl flex flex-col gap-5">
+        <div className="w-full h-30 flex items-center gap-3">
+          <div className="w-[30px] flex">
+            <img
+              src="/assets/icon2.png"
+              alt="donation icon"
+              style={{ transform: "scaleX(-1)" }}
+            />
+          </div>
+          <h3 className="text-[#1B4DFF] text-xl leading font-normal">
+            Donation Information
+          </h3>
+        </div>
+
+        <div className="bg-white p-[10px] rounded">
+          <p className="text-[#EB7101]">
+            By confirming this form, you will be providing your approval for the
+            concern team to deduct this amount of JOD from your next payroll(s).
+          </p>
+        </div>
+
+        <div className="w-full text-[#8EA6BF] flex flex-col gap-5">
+          <div className="w-full flex items-center justify-between gap-[60px]">
+            <div className="w-1/2">
+              <label className="text-sm">Organization</label>
+              <input
+                type="text"
+                value={selectedOrganization}
+                className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+              />
+            </div>
+            <div className="w-1/2">
+              <label className="text-sm">Donation Amount(JOD)</label>
+              <input
+                type="text"
+                value={donationAmount}
+                className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex items-center justify-between gap-[60px]">
+            <div className="w-1/2">
+              <label className="text-sm">Start Date</label>
+              <input
+                type="text"
+                value={startDate}
+                className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+              />
+            </div>
+            <div className="w-1/2">
+              <label className="text-sm">End Date</label>
+              <input
+                type="text"
+                value={endDate}
+                className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+              />
+            </div>
+          </div>
+
+          <div className="w-full flex items-center gap-1">
+            <input
+              type="checkbox"
+              id="recurring"
+              checked={isRecurring}
+              className="mr-2 w-4 h-4 accent-white focus:aaccent-[#1B4DFF]"
+            />
+            <label
+              className="text-[#1B4DFF] text-sm font-normal leading-[18px]"
+              htmlFor="recurring"
+            >
+              Recurring Donation
+            </label>
+          </div>
+
+          <div className="w-full flex items-center justify-center gap-4">
+            <button
+              className="w-[285px] px-6 py-3 text-sm font-medium text-[#8EA6BF] bg-white rounded-md border border-[#8EA6BF]"
+              onClick={closeModal}
+            >
+              Cancel
+            </button>
+            <button
+              onClick={handleConfirm}
+              className="w-[285px] px-6 py-3 text-sm font-medium text-white bg-[#1B4DFF] rounded-md"
+            >
+              Confirm
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SubmissionModal;
--- src/pages/Giving/index.jsx
+++ src/pages/Giving/index.jsx
@@ -1,14 +1,14 @@
-import { useState, useRef } from "react";
+import React, { useState, useRef } from "react";
+import Modal from "../../components/Modal";
 import { donationBeneficiaryList } from "./helper";
-
-import "./styles.css";
 
 const Giving = () => {
   const startDateRef = useRef(null);
   const endDateRef = useRef(null);
 
+  const [modalOpen, setModalOpen] = useState(false);
   const [selectedOrganization, setSelectedOrganization] = useState("");
-  const [donationAmount, setDonationAmount] = useState(null);
+  const [donationAmount, setDonationAmount] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [isRecurring, setIsRecurring] = useState(false);
@@ -55,14 +55,25 @@ const Giving = () => {
     setEndDate(formattedEndDate);
   };
 
-  const handleSubmit = async () => {
-    if (!selectedOrganization || !donationAmount || !startDate || !endDate) {
+  const handleSubmit = () => {
+    if (
+      !selectedOrganization ||
+      !donationAmount ||
+      !startDate ||
+      (!endDate && isRecurring)
+    ) {
       setFormSubmitted(true);
     } else {
       setFormSubmitted(false);
-      return;
+      setModalOpen(true);
     }
+  };
 
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleModalSubmit = async () => {
     const formData = {
       ETId: "",
       EmployeeProfileID: "1255",
@@ -95,117 +106,110 @@ const Giving = () => {
   };
 
   return (
-    <div className="w-full h-[1080px] flex items-center">
-      <div className="w-[354px] h-full bg-white">left</div>
-      <div className="w-full h-full bg-[#BCCAD9]">
-        <div className="w-full h-[120px] border-b border-[#a9bacd]"></div>
-        <div className="pt-[48px] pl-[20px] pr-[45px]">
-          <h2 className="text-2xl text-[#1B4DFF] leading-[18px] font-medium">
-            Donations
-          </h2>
-          <div className="w-full  bg-white mt-[53px] rounded-lg p-3">
-            <ul className="text-[#6989AA] capitalize list-disc space-y-4 list-inside pl-5">
-              <li>
-                The amount of the donation shall not exceed the 30% of your
-                salary.
-              </li>
-              <li>
-                In case of recurring donation, it will be deduction from every
-                payroll till the end of the selected period.
-              </li>
-              <li>The recurring donation shall not exceed the three months.</li>
-            </ul>
-          </div>
-          {/* Donation Form */}
-          <div className="w-full bg-[#F9FAFB] mt-[24px] pl-[60px] py-[68px] rounded-lg flex items-center gap-3">
-            {/* Donation Form Container */}
-            <div className="w-full">
-              {/* Donation Form Title */}
-              <h3 className="text-xl text-[#1B4DFF] leading-[18px] font-medium">
-                Donations Form
-              </h3>
-              {/* Organization Dropdown */}
-              <div className="w-[327px] pt-[50px]">
-                <label
-                  className="text-[#8EA6BF] text-sm leading-[18px] flex items-center gap-1"
-                  htmlFor="organization"
-                >
-                  Organization
-                  {formSubmitted && !selectedOrganization && (
-                    <span className="text-red-500 text-base">*</span>
-                  )}
-                </label>
-                <select
-                  required
-                  id="organization"
-                  value={selectedOrganization}
-                  onChange={handleOrganizationChange}
-                  className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-50`}
-                >
-                  <option className="text-xs disabled">Organization</option>
-                  {donationBeneficiaryList.map((org) => (
-                    <option
-                      key={org.donationBeneficiaryId}
-                      value={org.donationBeneficiaryId}
-                    >
-                      {org.donationBeneficiaryNameEn} (
-                      {org.donationBeneficiaryNameAr})
-                    </option>
-                  ))}
-                </select>
-              </div>
-              {/* Donation Amount Input */}
-              <div className="w-[327px] mt-4">
-                <label
-                  className="flex items-center gap-1 text-[#8EA6BF] text-sm leading-[18px]"
-                  htmlFor="donationAmount"
-                >
-                  Donation Amount(JD)
-                  {formSubmitted && !donationAmount && (
-                    <span className="text-red-500 text-base">*</span>
-                  )}
-                </label>
-                <input
-                  min={1}
-                  type="number"
-                  placeholder="Donation Amount(JD)"
-                  id="donationAmount"
-                  value={donationAmount}
-                  onChange={handleAmountChange}
-                  required
-                  className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
-                />
-              </div>
-              {/* Dates */}
-              <div className="w-full flex items-center justify-between gap-5 mt-3">
-                {/* Start date */}
-                <div className="w-[327px] flex flex-col justify-center">
+    <>
+      <div className="w-full h-[1080px] flex items-center">
+        <div className="w-[354px] h-full bg-white">left</div>
+        <div className="w-full h-full bg-[#BCCAD9]">
+          <div className="w-full h-[120px] border-b border-[#a9bacd]"></div>
+          <div className="pt-[48px] pl-[20px] pr-[45px]">
+            <h2 className="text-2xl text-[#1B4DFF] leading-[18px] font-medium">
+              Donations
+            </h2>
+            <div className="w-full bg-white mt-[53px] rounded-lg p-3">
+              <ul className="text-[#6989AA] capitalize list-disc space-y-4 list-inside pl-5">
+                <li>
+                  The amount of the donation shall not exceed the 30% of your
+                  salary.
+                </li>
+                <li>
+                  In case of recurring donation, it will be deduction from every
+                  payroll till the end of the selected period.
+                </li>
+                <li>
+                  The recurring donation shall not exceed the three months.
+                </li>
+              </ul>
+            </div>
+            <div className="w-full bg-[#F9FAFB] mt-[24px] pl-[60px] py-[68px] rounded-lg flex items-center gap-3">
+              <div className="w-full">
+                <h3 className="text-xl text-[#1B4DFF] leading-[18px] font-medium">
+                  Donations Form
+                </h3>
+                <div className="w-[327px] pt-[50px]">
+                  <label
+                    className="text-[#8EA6BF] text-sm leading-[18px] flex items-center gap-1"
+                    htmlFor="organization"
+                  >
+                    Organization
+                    {formSubmitted && !selectedOrganization && (
+                      <span className="text-red-500 text-base">*</span>
+                    )}
+                  </label>
+                  <select
+                    required
+                    id="organization"
+                    value={selectedOrganization}
+                    onChange={handleOrganizationChange}
+                    className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-50`}
+                  >
+                    <option className="text-xs disabled">Organization</option>
+                    {donationBeneficiaryList.map((org) => (
+                      <option
+                        key={org.donationBeneficiaryId}
+                        value={org.donationBeneficiaryNameEn}
+                      >
+                        {org.donationBeneficiaryNameEn}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="w-[327px] mt-4">
                   <label
                     className="flex items-center gap-1 text-[#8EA6BF] text-sm leading-[18px]"
-                    htmlFor="startDate"
+                    htmlFor="donationAmount"
                   >
-                    Start Date
-                    {formSubmitted && !startDate && (
+                    Donation Amount(JD)
+                    {formSubmitted && !donationAmount && (
                       <span className="text-red-500 text-base">*</span>
                     )}
                   </label>
                   <input
-                    ref={startDateRef}
-                    onFocus={() => (startDateRef.current.type = "date")}
-                    onBlur={() => (startDateRef.current.type = "date")}
+                    min={1}
+                    type="number"
+                    placeholder="Donation Amount(JD)"
+                    id="donationAmount"
+                    value={donationAmount}
+                    onChange={handleAmountChange}
                     required
-                    type="text"
-                    placeholder="Start Date"
-                    id="startDate"
-                    name="donation-start"
-                    value={startDate}
-                    onChange={handleStartDateChange}
-                    min={today}
-                    className={`w-full px-3 mt-2 py-2 bg-white fill-current text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+                    className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
                   />
                 </div>
-                {/* End date */}
-                {
+                <div className="w-full flex items-center justify-between gap-5 mt-3">
+                  <div className="w-[327px] flex flex-col justify-center">
+                    <label
+                      className="flex items-center gap-1 text-[#8EA6BF] text-sm leading-[18px]"
+                      htmlFor="startDate"
+                    >
+                      Start Date
+                      {formSubmitted && !startDate && (
+                        <span className="text-red-500 text-base">*</span>
+                      )}
+                    </label>
+                    <input
+                      ref={startDateRef}
+                      onFocus={() => (startDateRef.current.type = "date")}
+                      onBlur={() => (startDateRef.current.type = "date")}
+                      required
+                      type="text"
+                      placeholder="Start Date"
+                      id="startDate"
+                      name="donation-start"
+                      value={startDate}
+                      onChange={handleStartDateChange}
+                      min={today}
+                      className={`w-full px-3 mt-2 py-2 bg-white fill-current text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
+                    />
+                  </div>
                   <div className="w-[327px] flex flex-col justify-center">
                     <label
                       className="flex items-center gap-1 text-[#8EA6BF] text-sm leading-[18px]"
@@ -229,52 +233,51 @@ const Giving = () => {
                       className={`w-full px-3 mt-2 py-2 bg-white text-[#8EA6BF] border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500`}
                     />
                   </div>
-                }
-                {/* Recurring Checkbox */}
-                <div className="w-[166px] flex items-center justify-center mt-5">
-                  <input
-                    type="checkbox"
-                    id="recurring"
-                    checked={isRecurring}
-                    onChange={handleRecurringChange}
-                    className="mr-2 w-4 h-4 accent-white focus:aaccent-[#1B4DFF]"
-                  />
-                  <label
-                    className="text-[#1B4DFF] text-sm font-normal leading-[18px]"
-                    htmlFor="recurring"
+                  <div className="w-[166px] flex items-center justify-center mt-5">
+                    <input
+                      type="checkbox"
+                      id="recurring"
+                      checked={isRecurring}
+                      onChange={handleRecurringChange}
+                      className="mr-2 w-4 h-4 accent-white focus:aaccent-[#1B4DFF]"
+                    />
+                    <label
+                      className="text-[#1B4DFF] text-sm font-normal leading-[18px]"
+                      htmlFor="recurring"
+                    >
+                      Recurring Donation
+                    </label>
+                  </div>
+                </div>
+                {formSubmitted && (
+                  <p className={`text-red-400 text-xs mt-[12px]`}>
+                    Please fill all the required fields
+                  </p>
+                )}
+                <div className="w-[327px] mt-[18px]">
+                  <button
+                    onClick={handleSubmit}
+                    className="w-full mt-[12px] px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600"
                   >
-                    Recurring Donation
-                  </label>
+                    Submit
+                  </button>
                 </div>
               </div>
-
-              {formSubmitted && (
-                <p className={`text-red-400 text-xs mt-[12px]`}>
-                  Please fill all the required fields
-                </p>
-              )}
-              <div className="w-[327px] mt-[18px]">
-                <button
-                  onClick={handleSubmit}
-                  className="w-full mt-[12px] px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
-            {/* Image */}
-            <div className="w-2/4 text-black flex justify-end">
-              <img
-                width={285}
-                height={274.92}
-                src="../../public/assets/icon2.png"
-                alt=""
-              />
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <Modal
+        isOpen={modalOpen}
+        closeModal={closeModal}
+        selectedOrganization={selectedOrganization}
+        donationAmount={donationAmount}
+        startDate={startDate}
+        endDate={endDate}
+        isRecurring={isRecurring}
+        handleSubmit={handleModalSubmit}
+      />
+    </>
   );
 };
 
